ddb29c6a47b1299efc9d7b6567a312e35b9d9386
Shivam Kumar <93670358+Shivamkr02@users.noreply.github.com>
2024-12-09 08:33:46 +0530

    Add files via upload
    Define a function to check whether a given number is a prime number or not.

---
+++ question11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+bool check_prime(int n){
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return false;
+        } 
+    }
+    return true;
+}
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(check_prime(n)){
+        cout<<n<<"is prime number"<<endl;
+    }else{
+        cout<<n<<"is not prime number"<<endl;
+    }
+
+}
