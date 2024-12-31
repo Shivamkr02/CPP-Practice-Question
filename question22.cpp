7b90abdf59f3184f10ebf90f0a09a7f4d8733d69
shivamkr02 <shivamvts05@gmail.com>
2024-12-31 22:00:00 +0530

    Define a function to sort an array of strings in ascending or descending order depending on bool type argument (true for ascending and false for descending). Use default argument to implement it.

---
+++ question22.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+void sort(char *str[],int n,bool asc){
+    int i,r;
+    char temp[6];
+    if(asc){
+        for(r=0;r<=n-1;r++){
+            for(i=0;i<=n-1;i++){
+                if(strcmp(str[i],str[i+1])>0){
+                    strcpy(temp,str[i]);
+                    strcpy(str[i],str[i+1]);
+                    strcpy(str[i+1],temp);
+                }
+            }
+        }
+    }
+    else{
+        for(r=0;r<=n-1;r++){
+            for(i=0;i<=n-1;i++){
+                if(strcmp(str[i],str[i+1])>0){
+                    strcpy(temp,str[i]);
+                    strcpy(str[i],str[i+1]);
+                    strcpy(str[i+1],temp);
+                }
+            }
+        }
+    }
+}
+int main(){
+
+    
+    return 0;
+}
