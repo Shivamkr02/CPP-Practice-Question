9580c28ce5327b15d8248871a33596dc77cd56ac
shivamkr02 <shivamvts05@gmail.com>
2025-02-28 23:57:23 +0530

    commit

--- question43.cpp
+++ question43.cpp
@@ -14,6 +14,31 @@ class Time
             sec=z;
         }
         void showTime(){
-            cout<<""
+            cout<<"hour"<<" : "<<min<<": "<<sec<<endl;
+        }
+        bool operator>(Time t){
+            if(hour>t.hour)
+                return true;
+            else if(hour<t.hour)
+                return false;
+            else if(min>t.min)
+                return true;
+            else if(min<t.min)
+                return false;
+            else if(sec>t.sec)
+                return true;
+            else if(sec<t.sec)
+                return false;
+        }
+        Time operator++(){ // Pre-Increment
+            Time temp =* this;
+            sec++;
+            min=min+sec/60;
+            hour=hour+min/60;
+            min=min%60;
+            return temp;
         }
 };
+int main(){
+    Time t1,t2,t3;
+}
