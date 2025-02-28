66e7f22cff275eb65ef849ecf91b62cda8c54ae7
shivamkr02 <shivamvts05@gmail.com>
2025-02-28 22:30:05 +0530

    commit

--- question42.cpp
+++ question42.cpp
@@ -13,7 +13,7 @@ class Time
             second=z;
         }
         void showTime(){
-            cout<<hour<<" : "<<min<<" : "<<second<<" : ";
+            cout<<hour<<" : "<<min<<" : "<<second;
         }
         bool operator>(Time t){
             if(hour>t.hour)
@@ -35,6 +35,10 @@ int main(){
     Time t1,t2,t3;
     t1.setTime(07,53,55);
     t2.setTime(06,35,53);
+    t1.showTime();
+    cout<<endl;
+    t2.showTime();
+    cout<<endl;
     cout<<t1.operator>(t2);
 
     return 0;
---
+++ question43.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;  
+
+class Time
+{
+    private:
+        int hour;
+        int min;
+        int sec;
+    public:
+        void setTime(int x,int y,int z){
+            hour=x;
+            min=y;
+            sec=z;
+        }
+        void showTime(){
+            cout<<""
+        }
+};
