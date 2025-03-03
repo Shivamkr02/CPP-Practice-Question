a3e4c7ebbef9daf671f1480514af101cc198f8f2
shivamkr02 <shivamvts05@gmail.com>
2025-03-03 22:54:10 +0530

    commit

---
+++ question54.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+    private:
+        int hr,min,sec;
+    public:
+        void setTime(int hr,int min,int sec){
+            this->hr=hr;
+            this->min=min;
+            this->sec=sec;
+        }
+        void showTime(){
+            cout<<hr<<" :"<<min<<" :"<<sec;
+        }
+        void setHour(int hr){this->hr=hr;}
+        void setMinutes(int min){this->min=min;}
+        void setSeconds(int sec){this->sec=sec;}
+        int getHours(){return hr;}
+        int getMinutes(){return min;}
+        int getSeconds(){return sec;}
+
+};
+int main(){
+    Time d1;
+    d1.setTime(05,45,23);
+    d1.showTime();
+    return 0;
+}
