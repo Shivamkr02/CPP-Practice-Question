d4ecab13832944c8c1f59931dfb49dc7f227e753
shivamkr02 <shivamvts05@gmail.com>
2025-04-01 22:42:52 +0530

    commit

---
+++ Array2/findUnique.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,1,4,2,3,4,5,5};
+    int n = sizeof(arr)/4;
+    for(int i=0;i<n;i++){
+        bool flag = true; // true means unique
+        for(int j=0;j<n;j++){
+            if(j==i) continue;
+            if(arr[i]==arr[j]){ // not unique
+                flag = false;
+                break;
+            }
+        }
+        if(flag==true){ // arr[i] is unique
+            cout<<arr[i]<<endl;
+            break;
+        }
+    }
+}
