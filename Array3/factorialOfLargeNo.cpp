d146a9f45f681368d790d04687ae5916d07d786b
shivamkr02 <shivamvts05@gmail.com>
2025-04-05 00:01:03 +0530

    commit

--- Array3/factorialOfLargeNo.cpp
+++ Array3/factorialOfLargeNo.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 
 int main(){
-    
+    int n;
+    cout<<"Enter the number to find factorial: ";
+    cin>>n;
+    int fact=1;
+    for(int i=1;i<=5;i++){
+        fact*=i;
+    }
+    cout<<fact;
+
+    return 0;
 }
